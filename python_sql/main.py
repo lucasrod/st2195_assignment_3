79366c34ca6d836eebc7fb5fb18fd7b101ef1981
Lucas Rodriguez <lucas.rodriguez@outlook.com>
2023-12-15 19:41:33 -0300

    Initial main script

--- python_sql/main.py
+++ python_sql/main.py
@@ -4,13 +4,49 @@
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
+def main():
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    # Define some initial values for the dataset and database files
+    required_files = [
+        "2000.csv.bz2", "2001.csv.bz2", "2002.csv.bz2",
+        "2003.csv.bz2", "2004.csv.bz2", "2005.csv.bz2",
+        "airports.csv", "carriers.csv", "plane-data.csv"
+    ]
+
+    database_path = "database/airline2.db"
+
+    # Define table names and corresponding CSV file paths
+    tables_data = {
+        "ontime": ["data/2000.csv.bz2", "data/2001.csv.bz2",
+                   "data/2002.csv.bz2", "data/2003.csv.bz2",
+                   "data/2004.csv.bz2", "data/2005.csv.bz2"],
+        "airports": "data/airports.csv",
+        "carriers": "data/carriers.csv",
+        "planes": "data/plane-data.csv"
+    }
+
+    # Execute the workflow
+    print("Downloading Data Expo 2009: Airline Time Dataset\n") # Press ⌘F8 to toggle the breakpoint.
+    download_data.download(required_files)
+
+    # Build SQLite db file and tables
+    if not os.path.exists(database_path):
+        print(f"Building {database_path}\n")
+        construct_db.init_db(database_path)
+
+        # Load the actual data
+        print("Loading the data\n")
+        construct_db.load_data(database_path, tables_data)
+
+    print("Executing queries...\n")
+    queries_sqlite.execute_queries(database_path)
+    queries_orm.execute_queries(database_path)
+
+    print("Assignment 3 completed\n")
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
